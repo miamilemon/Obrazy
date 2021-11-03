82c168496ac18720172d6413d1bb489e678ea41f
miamilemon <kontakt.thexenonz@gmail.com>
2021-11-03 19:23:31 +0100

    wszystko oprocz usuwania i uzywania kamery

--- app/src/main/java/com/example/obrazy/MainActivity.kt
+++ app/src/main/java/com/example/obrazy/MainActivity.kt
@@ -2,10 +2,78 @@ package com.example.obrazy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.widget.doOnTextChanged
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var img = findViewById<ImageView>(R.id.imageViewZdj)
+        var widok = findViewById<CheckBox>(R.id.checkBoxWidok)
+        var lewo = findViewById<Button>(R.id.buttonLeft)
+        var prawo = findViewById<Button>(R.id.buttonPrawo)
+        var usun = findViewById<Button>(R.id.buttonClear)
+        var rotacja = findViewById<EditText>(R.id.editTextRotacja)
+        var skala = findViewById<EditText>(R.id.editTextSkala)
+        var currentlyselected = 0;
+        val zdjecia = arrayOf(R.drawable.musztarda, R.drawable.szef, R.drawable.szefsyn2,R.drawable.zupka)
+
+        widok.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(widok.isChecked()){
+                img.setVisibility(View.INVISIBLE)
+            }else{
+                img.setVisibility(View.VISIBLE)
+            }
+        }
+
+        prawo.setOnClickListener {
+            currentlyselected++;
+            if(currentlyselected==4){
+                currentlyselected = 0;
+            }
+            img.setImageResource(zdjecia[currentlyselected])
+        }
+
+        lewo.setOnClickListener {
+            currentlyselected--;
+            if(currentlyselected==-1){
+                currentlyselected = 3;
+            }
+            img.setImageResource(zdjecia[currentlyselected])
+        }
+
+        /*usun.setOnClickListener{
+            zdjecia[currentlyselected] = null ;
+        }*/
+
+        rotacja.doOnTextChanged { text, start, before, count ->
+            var wartosc = rotacja.text.toString()
+            if(wartosc.isNotEmpty()) {
+                var floatwartosc = wartosc.toFloat()
+                img.rotation = floatwartosc
+            }
+        }
+
+        skala.doOnTextChanged { text, start, before, count ->
+            var wartosc = skala.text.toString()
+            if(wartosc.isNotEmpty()) {
+                var floatwartosc = wartosc.toFloat()
+                img.scaleX = floatwartosc
+                img.scaleY = floatwartosc
+            }
+        }
+
+
     }
+
 }
